0706b67ae1ba065aa137f8a619083877426504b4
skmmarine <loyjs@naver.com>
2016-02-13 23:23:06 +0900

    new upload

--- hello/models.py
+++ hello/models.py
@@ -1,32 +1,7 @@
 import datetime
-<<<<<<< Updated upstream
 
 from django.db import models
 from django.utils import timezone
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
-=======
-
-from django.db import models
-from django.utils import timezone
-# Create your models here.
 
 class Question(models.Model): #표 하나에 열을 추가할 수 잇음
     question_text = models.CharField(max_length=200)
@@ -48,4 +23,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
->>>>>>> Stashed changes
