d0771ba9d36574129964a36f9a81be764ffab1b9
skmmarine <loyjs@naver.com>
2016-02-25 18:48:57 +0900

    "make_login_1"

--- poll/urls.py
+++ poll/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
     #url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 
+     url(r'login/$', views.SignInView.as_view(),name='login'),
+
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(),name='results'),
     #url(r'^(?P<question_id>[0-9]+)/results/$',views.results, name='results'),
 
